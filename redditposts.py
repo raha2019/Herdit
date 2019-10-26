f46ed47f52441f715f8439ccd22dbbeff1674064
Rahul Gupta <rguptaemail10@gmail.com>
2019-10-26 16:17:36 -0400

    Got the mp3 file working

--- redditposts.py
+++ redditposts.py
@@ -1,25 +1,28 @@
 import praw
+from gtts import gTTS
+import os
 
 
 def RedditPostAutoReader(subreddit):
     reddit = praw.Reddit(client_id='zQkL7caN9akLQw', client_secret='XXkY9kxR42u5LnhoJcGKe3YTtSw', user_agent='RedditPostAutoReader')
-    popular_posts = reddit.subreddit(subreddit).hot(limit=3)
+    popular_posts = reddit.subreddit(subreddit).hot(limit=5)
     f = open("redditPost.txt", "w+")
     for post in popular_posts:
         title = (post.title)
         paragraph = (post.selftext)
-        textPost = (post.is_self)
-        # if textPost:
-        #      return title
-        # else:
-        #     print(title)
-        # return paragraph
-        #print("didn't work")
-    # print(post.upvote_ratio
+    # print(post.upvote_ratio)
         f.write(title+"\n" + paragraph+"\n")
+        tts = gTTS(text=title + paragraph, lang='en')
+        tts.save("result.mp3")
+        os.system("mpg321 result.mp3")
     f.close()
 
-
+def checkIfTextOrImage():
+    textPost = (post.is_self)
+    if textPost == "True":
+          return title
+    else:
+         print("it didn't work")
 
 if __name__ == '__main__':
     user_input = "nosleep"
