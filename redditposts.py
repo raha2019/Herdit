80fcae82a4a7bd026990033ecc0fea98f35a9f9e
jacksonmerg <jacksonmerg@gmail.com>
2019-10-26 15:18:46 -0400

    Update redditposts.py

--- redditposts.py
+++ redditposts.py
@@ -4,17 +4,17 @@ import praw
 
 
 
-def RedditPostAutoReader():
-    print(input("Enter Reddit Post"))
+def RedditPostAutoReader(subreddit):
     reddit = praw.Reddit(client_id='zQkL7caN9akLQw', client_secret='XXkY9kxR42u5LnhoJcGKe3YTtSw', user_agent='RedditPostAutoReader')
-    popular_posts = reddit.subreddit('AskReddit').hot(limit=1)
+    popular_posts = reddit.subreddit(subreddit).hot(limit=1)
     for post in popular_posts:
         title = (post.title)
-        post = (post.selftext)
-        if post.is_self == True:
+        text = (post.selftext)
+        if post.is_self():
             print(title)
-            print(post)
-    # print(post.upvote_ratio)
+            print(text)
+            return text
+    # print(post.upvote_ratio
     f = open("demofile2.txt", "a")
     f.write("Now the file has more content!")
     f.close()
