d0ada4ae3138ea177dd413c2da4dff84287709d5
Rahul Gupta <rguptaemail10@gmail.com>
2019-10-26 18:21:10 -0400

    fixed some stuff

--- redditposts.py
+++ redditposts.py
@@ -8,17 +8,55 @@ from pygame.locals import *
 def pygameVideo():
 
     pygame.init()
-    pygame.camera.init()
 
-    try:
-        os.makedirs("Snaps")
-    except OSError:
-        pass
-    screen = pygame.display.set_mode((640, 480))
+    white = (255, 255, 255)
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
 
-    cam = pygame.camera.Camera("/dev/video0", (640, 480))
-    cam.start()
-    print("")
+    X = 400
+    Y = 400
+
+    display_surface = pygame.display.set_mode((X, Y ))
+
+# set the pygame window name
+    pygame.display.set_caption('First Frame')
+
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    text = font.render('redditPost.txt', True, green, blue)
+
+    textRect = text.get_rect()
+
+    textRect.center = (X // 2, Y // 2)
+
+    while True :
+
+        display_surface.fill(white)
+
+        display_surface.blit(text, textRect)
+
+        for event in pygame.event.get() :
+
+            if event.type == pygame.QUIT :
+
+                pygame.quit()
+
+                quit()
+
+            pygame.display.update()
+    #
+    # pygame.init()
+    # pygame.camera.init()
+    #
+    # try:
+    #     os.makedirs("Snaps")
+    # except OSError:
+    #     pass
+    # screen = pygame.display.set_mode((640, 480))
+    #
+    # cam = pygame.camera.Camera("/dev/video0", (640, 480))
+    # cam.start()
+    # print("")
 
 
 def RedditPostAutoReader(subreddit):
