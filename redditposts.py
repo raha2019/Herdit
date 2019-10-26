9a0fa6efe595bc7fc9968c8ee5b3418d058b3b43
Rahul Gupta <rguptaemail10@gmail.com>
2019-10-26 15:51:37 -0400

    It works

--- redditposts.py
+++ redditposts.py
@@ -1,23 +1,27 @@
 import praw
 
 
-
-
-
 def RedditPostAutoReader(subreddit):
     reddit = praw.Reddit(client_id='zQkL7caN9akLQw', client_secret='XXkY9kxR42u5LnhoJcGKe3YTtSw', user_agent='RedditPostAutoReader')
-    popular_posts = reddit.subreddit(subreddit).hot(limit=1)
+    popular_posts = reddit.subreddit(subreddit).hot(limit=3)
+    f = open("redditPost.txt", "w+")
     for post in popular_posts:
         title = (post.title)
         paragraph = (post.selftext)
-        #textPost = (post.is_self)
+        textPost = (post.is_self)
         # if textPost:
-        #     return title
+        #      return title
         # else:
-        print(title)
-        return paragraph
+        #     print(title)
+        # return paragraph
         #print("didn't work")
     # print(post.upvote_ratio
-    f = open("redditPost.txt", "a")
-    f.write("Now the file has more content!")
+        f.write(title+"\n" + paragraph+"\n")
     f.close()
+
+
+
+if __name__ == '__main__':
+    user_input = "nosleep"
+    subreddit = user_input
+    RedditPostAutoReader(subreddit)
