93711cd7b44b7d7e6dcc9f640dbe117676094ebd
Rahul Gupta <rguptaemail10@gmail.com>
2019-10-26 15:14:54 -0400

    Function

--- redditposts.py
+++ redditposts.py
@@ -1,10 +1,20 @@
 import praw
 
-reddit = praw.Reddit(client_id='zQkL7caN9akLQw', client_secret='XXkY9kxR42u5LnhoJcGKe3YTtSw', user_agent='RedditPostAutoReader')
 
-hot_posts = reddit.subreddit('AskReddit').hot(limit=1)
-for post in hot_posts:
-    print(post.title)
-    print(post.upvote_ratio)
-    print(post.is_self)
-    print(post.selftext)
+
+
+
+def RedditPostAutoReader():
+    print(input("Enter Reddit Post"))
+    reddit = praw.Reddit(client_id='zQkL7caN9akLQw', client_secret='XXkY9kxR42u5LnhoJcGKe3YTtSw', user_agent='RedditPostAutoReader')
+    popular_posts = reddit.subreddit('AskReddit').hot(limit=1)
+    for post in popular_posts:
+        title = (post.title)
+        post = (post.selftext)
+        if post.is_self == True:
+            print(title)
+            print(post)
+    # print(post.upvote_ratio)
+    f = open("demofile2.txt", "a")
+    f.write("Now the file has more content!")
+    f.close()
